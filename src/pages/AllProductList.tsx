b1b96bff1fe57d99607b28a38ed59a4cee09198b
Sagar-006 <sagarbiradar7030@gmail.com>
2025-07-24 21:43:02 +0530

    backend url added in env

--- src/pages/AllProductList.tsx
+++ src/pages/AllProductList.tsx
@@ -50,10 +50,6 @@ const AllProductList = () => {
           <Navbar />
 
           <main className=" flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-            {/* <h1 className="text-2xl font-bold mb-4 capitalize">
-              Showing{" "}
-              {category === "ALL" ? "All Products" : category + " Products"}
-            </h1> */}
 
             <div className=" grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
               {allProducts.map((item) => (
